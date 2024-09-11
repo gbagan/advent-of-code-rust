a9994316bb72cd0b56df7732901935abe086d4cc
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2024-09-11 14:33:27 +0200

    2023: optimize day 17

--- src/year2023/day17.rs
+++ src/year2023/day17.rs
@@ -3,10 +3,10 @@
 use anyhow::*;
 use crate::util::{coord::Coord, grid::Grid};
 
-type Point = Coord<i32>;
+type Point = Coord<usize>;
 
 pub fn solve(input: &str) -> Result<(u16, u16)> {
-    let grid= Grid::parse(input)?;
+    let grid= Grid::parse_with_padding(input, b'#')?;
     let p1 = astar(&grid, 1, 3).context("Part 1: No solution found")?;
     let p2 = astar(&grid, 4, 10).context("Part 2: No solution found")?;
     Ok((p1, p2))
@@ -15,20 +15,27 @@ pub fn solve(input: &str) -> Result<(u16, u16)> {
 const VERTICAL: usize = 0;
 const HORIZONTAL: usize = 1;
 
-fn astar(grid: &Grid<u8>, min_dist: u16, max_dist: u16) -> Option<u16> {
-    let start= Point::ORIGIN;
-    let goal = Point::new(grid.width as i32 - 1, grid.height as i32 - 1);
-    let heuristic = grid.map_with_indices(|p, _| {
-        let diff = goal - p;
-        let dist = p.manhattan(goal);
-        let penalty = diff.x.abs_diff(diff.y) as u16 * min_dist / max_dist;
-        dist as usize + penalty as usize
-    });
-    let max_heuristic = heuristic.vec.iter().max()?;
+fn astar(grid: &Grid<u8>, min_dist: usize, max_dist: usize) -> Option<u16> {
+    let start = grid.width + 1;
+    let goal = Point::new(grid.width - 2, grid.height - 2);
+    let mut heuristic = vec![0; grid.width * grid.height];
+    for y in 1..grid.height - 1 {
+        for x in 1..grid.width - 1 {
+            let p = Point::new(x, y);
+            let diff = goal - p;
+            let dist = p.manhattan(goal);
+            let penalty = diff.x.abs_diff(diff.y) * min_dist / max_dist;
+            heuristic[y * grid.width + x] = dist + penalty;
+        }
+    }
+    
+    let max_heuristic = heuristic.iter().max()?;
     let mut queue = BucketQueue::new(*max_heuristic + 100);
     let mut costs = Grid::new(grid.width, grid.height, [u16::MAX; 2]);
     queue.insert(heuristic[start], (start, HORIZONTAL, 0));
     queue.insert(heuristic[start], (start, VERTICAL, 0));
+
+    let goal = (grid.height - 1) * grid.width - 2;
     while let Some((_, (node, direction, cost))) = queue.pop() {
         if node == goal {
             return Some(cost);
@@ -38,18 +45,17 @@ fn astar(grid: &Grid<u8>, min_dist: u16, max_dist: u16) -> Option<u16> {
         }
 
         let dirs = if direction == VERTICAL {
-            [Point::NORTH, Point::SOUTH]}
+            [0usize.wrapping_sub(grid.width), grid.width]}
         else {
-            [Point::WEST, Point::EAST]
+            [usize::MAX, 1] // [-1, 1]
         };
-        
         for dir2 in dirs {
             let mut psum = 0;
             let mut next = node;
             let next_dir = 1 - direction;
             for i in 1..max_dist+1 {
                 next += dir2;
-                if !grid.contains(next) {
+                if grid[next] == b'#' {
                     break;
                 }
                 psum += grid[next] - b'0';
