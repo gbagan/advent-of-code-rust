4d4239bd8c360182b867d2449c7202977a9dfb57
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2024-12-04 17:36:33 +0100

    2024 day 4, improve performance

--- src/year2024/day04.rs
+++ src/year2024/day04.rs
@@ -8,66 +8,26 @@ pub fn solve(input: &str) -> Result<(u32, u32)> {
     Ok((p1, p2))
 }
 
+const DIRECTIONS: [(usize, usize); 8] = [(0, 1), (1, 0), (1, 1), (1, usize::MAX)
+                , (0, usize::MAX), (usize::MAX, 0), (usize::MAX, usize::MAX), (usize::MAX, 1)];
+
 fn part1(grid: &Grid<u8>) -> u32 {
-    let vec = &grid.vec;
     let height = grid.height;
     let width = grid.width;
     let mut count = 0;
 
-    for i in 0..height {
-        for j in 0..width - 3 {
-            let index = i * width + j;
-            if vec[index] == b'X' && vec[index+1] == b'M' 
-                && vec[index+2] == b'A' && vec[index+3] == b'S' {
-                count += 1;
-            }
-            if vec[index] == b'S' && vec[index+1] == b'A' 
-                && vec[index+2] == b'M' && vec[index+3] == b'X' {
-                count += 1;
-            }
-        }
-    }
-
-    for i in 0..height - 3 {
-        for j in 0..width {
-            let index = i * width + j;
-            if vec[index] == b'X' && vec[index+width] == b'M' 
-                && vec[index+2*width] == b'A' && vec[index+3*width] == b'S' {
-                count += 1;
-            }
-            if vec[index] == b'S' && vec[index+width] == b'A' 
-                && vec[index+2*width] == b'M' && vec[index+3*width] == b'X' {
-                count += 1;
-            }
-        }
-    }
-
-    for i in 0..height - 3 {
-        for j in 0..width - 3 {
-            let index = i * width + j;
-            let d = width + 1;
-            if vec[index] == b'X' && vec[index+d] == b'M' 
-                && vec[index+2*d] == b'A' && vec[index+3*d] == b'S' {
-                count += 1;
-            }
-            if vec[index] == b'S' && vec[index+d] == b'A' 
-                && vec[index+2*d] == b'M' && vec[index+3*d] == b'X' {
-                count += 1;
-            }
-        }
-    }
-
-    for i in 0..height - 3 {
-        for j in 3..width {
-            let index = i * width + j;
-            let d = width - 1;
-            if vec[index] == b'X' && vec[index+d] == b'M' 
-                && vec[index+2*d] == b'A' && vec[index+3*d] == b'S' {
-                count += 1;
-            }
-            if vec[index] == b'S' && vec[index+d] == b'A' 
-                && vec[index+2*d] == b'M' && vec[index+3*d] == b'X' {
-                count += 1;
+    for x in 0..height {
+        for y in 0..width {
+            let cur = grid[(x, y)];
+            if cur == b'X' {
+                for (dx, dy) in DIRECTIONS {
+                    if x+3*dx < height && y+3*dy < width 
+                        && grid[(x+dx, y+dy)] == b'M' 
+                        && grid[(x+2*dx, y+2*dy)] == b'A' 
+                        && grid[(x+3*dx, y+3*dy)] == b'S' {
+                            count += 1;
+                    }
+                }
             }
         }
     }
