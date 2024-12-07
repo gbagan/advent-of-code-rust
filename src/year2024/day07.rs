b2a26cd7d7e8be15927fcad56f8da8d81edb64da
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2024-12-07 12:22:17 +0100

    2024 day 7: optimize code

--- src/year2024/day07.rs
+++ src/year2024/day07.rs
@@ -1,19 +1,21 @@
 use anyhow::*;
-use itertools::Itertools;
 use crate::util::parser::*;
 
 pub fn solve(input: &str) -> Result<(u64, u64)> {
     let mut p1 = 0;
     let mut p2 = 0;
 
+    let mut row = Vec::new();
+
     for line in input.lines() {
-        let row = line.iter_unsigned::<u64>().collect_vec();
+        row.extend(line.iter_unsigned::<u64>());
         if solve_p1(&row, row[0],row.len()-1) {
             p1 += row[0];
         }
         if solve_p2(&row, row[0],row.len()-1) {
             p2 += row[0];
         }
+        row.clear();
     }
 
     Ok((p1, p2))
@@ -46,9 +48,17 @@ pub fn solve_p2(row: &[u64], goal: u64, idx: usize) -> bool {
     goal >= current && solve_p2(row, goal - current, idx - 1)
 }
 
+#[inline]
 fn truncate_number(x: u64, y: u64) -> Option<u64> {
-    let n = y.ilog10();
-    let m = 10u64.pow(n+1);
+    let m =
+        if y < 10 {
+            10
+        } else if y < 100 {
+            100
+        } else {
+            1000
+        };
+    
     if x % m == y {
         Some(x / m)
     } else {
