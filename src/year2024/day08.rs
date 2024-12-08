b1d3a15f7dcb33e6eba7a1dee65e6171d98133b0
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2024-12-08 10:06:14 +0100

    2024 day 8

--- src/year2024/day08.rs
+++ src/year2024/day08.rs
@@ -1,9 +1,70 @@
 use anyhow::*;
-use itertools::Itertools;
-use crate::util::parser::*;
+use crate::util::{coord::*, grid::*};
 
-pub fn solve(input: &str) -> Result<(u32, u32)> {
-    let mut p1 = 0;
-    let mut p2 = 0;
+pub fn solve(input: &str) -> Result<(usize, usize)> {
+    let mut grid = Grid::parse(input)?;
+    let width = grid.width as i32;
+    let height = grid.height as i32;
+
+    let mut antennas: Vec<Vec<Coord<i32>>> = vec![Vec::new(); 256];
+    for y in 0..height {
+        for x in 0..width {
+            let c = grid[Coord::new(x, y)];
+            if c != b'.' {
+                antennas[c as usize].push(Coord::new(x, y))
+            }
+        }
+    }
+
+    for freq in &antennas {
+        if freq.len() < 2 {
+            continue;
+        }
+        for (i, antenna1) in freq[0..freq.len()-1].iter().enumerate() {
+            for antenna2 in freq[i + 1 ..].iter() {
+                let diff = *antenna2 - *antenna1;
+                let pos1 = *antenna1 - diff;
+                if grid.contains(pos1) {
+                    grid[pos1] = b'#';
+                }
+                let pos2 = *antenna2 + diff;
+                if grid.contains(pos2) {
+                    grid[pos2] = b'#';
+                }
+            }
+        }
+    }
+
+    let p1 = grid.vec.iter().filter(|&&c| c == b'#').count();
+
+    for freq in &antennas {
+        if freq.len() < 2 {
+            continue;
+        }
+        for &antenna in freq {
+            grid[antenna] = b'#';
+        }
+        
+        for (i, antenna1) in freq[0..freq.len()-1].iter().enumerate() {
+            for antenna2 in freq[i + 1 ..].iter() {
+                let diff = *antenna2 - *antenna1;
+                
+                let mut pos1 = *antenna1 - diff - diff;
+                let mut pos2 = *antenna2 + diff + diff;
+                while grid.contains(pos1) {
+                    grid[pos1] = b'#';
+                    pos1 = pos1 - diff;
+                }
+                while grid.contains(pos2) {
+                    grid[pos2] = b'#';
+                    pos2 = pos2 + diff;
+                }
+            }
+        }
+    }
+
+    let p2= grid.vec.iter().filter(|&&c| c == b'#').count();
+
     Ok((p1, p2))
+
 }
