673491ab877d71a9ea5f3ef853178ae4ab117afc
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2024-12-19 19:56:41 +0100

    2024 day 19, code cleaning

--- src/year2024/day19.rs
+++ src/year2024/day19.rs
@@ -1,7 +1,7 @@
 use anyhow::*;
 
 pub fn solve(input: &str) -> Result<(u32, u64)> {
-    let split = memchr::memmem::find(input.as_bytes(), b"\n\n").context("No separator found")?;
+    let split = memchr::memmem::find(input.as_bytes(), b"\n\n").context("Parsing error: No separator found")?;
     let patterns = &input[..split];
     let designs = &input[split+2..];
     
@@ -13,21 +13,19 @@ pub fn solve(input: &str) -> Result<(u32, u64)> {
     let mut p1 = 0;
     let mut p2 = 0;
 
-    for design in designs.lines() {
-        let score = trie.count(design.as_bytes());
-        if score > 0 {
-            p1 += 1;
-        }
+    let mut table = Vec::with_capacity(75);
+
+    for design in designs.lines() { 
+        let score = trie.count(design.as_bytes(), &mut table);
+        p1 += (score > 0) as u32;
         p2 += score;
     }
     Ok((p1, p2))
 }
 
- 
-#[derive(Debug)]
 struct TrieNode {
     here: bool,
-    children: [usize; 5]
+    children: [u16; 5]
 }
 
 struct Trie {
@@ -44,52 +42,53 @@ impl Trie {
     fn insert(&mut self, word: &[u8]) {
         let mut node_index =  0;
         for &c in word {
-            let idx = TABLE[c as usize];
+            let idx = COLOR_INDEX[c as usize];
             match self.nodes[node_index].children[idx] { 
                 0 => {
                     let size = self.nodes.len();
                     self.nodes.push(TrieNode { here: false, children: [0; 5] });
-                    self.nodes[node_index].children[idx] = size;
+                    self.nodes[node_index].children[idx] = size as u16;
                     node_index = size;
                 },
-                n => node_index = n
+                n => node_index = n as usize
             }
         }
         self.nodes[node_index].here = true;
     }
 
-    fn count(&self, design: &[u8]) -> u64 {
-        let mut cache = vec![0; design.len()+1];
-        cache[0] = 1;
+    fn count(&self, design: &[u8], table: &mut Vec<u64>) -> u64 {
+        table.clear();
+        table.resize(design.len() + 1, 0);
+        table[0] = 1;
         for i in 0..design.len() {
-            if cache[i] != 0 {
+            if table[i] != 0 {
                 let mut node_index = 0;
                 let mut index = i;
                 loop {
                     if self.nodes[node_index].here {
-                        cache[index] += cache[i];
+                        table[index] += table[i];
                     }
                     if index >= design.len() {
                         break;
                     }
-                    match self.nodes[node_index].children[TABLE[design[index] as usize]] {
+                    match self.nodes[node_index].children[COLOR_INDEX[design[index] as usize]] {
                         0 => break,
-                        idx => { node_index = idx; index += 1 }
+                        idx => { node_index = idx as usize; index += 1 }
                     }
                 }
             }
         }
-        cache[design.len()]
+        table[design.len()]
     }
 }
 
-const fn mk_table() -> [usize; 128] {
-    let mut table = [0; 128];
-    table[b'w' as usize] = 1;
-    table[b'u' as usize] = 2;
-    table[b'b' as usize] = 3;
-    table[b'r' as usize] = 4;
-    table
+const fn mk_index() -> [usize; 128] {
+    let mut index = [0; 128];
+    index[b'w' as usize] = 1;
+    index[b'u' as usize] = 2;
+    index[b'b' as usize] = 3;
+    index[b'r' as usize] = 4;
+    index
 }
 
-const TABLE: [usize; 128] = mk_table();
+const COLOR_INDEX: [usize; 128] = mk_index();
