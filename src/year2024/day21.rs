b0907fe0810f47744841932a1612182398d43a06
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2024-12-21 20:17:27 +0100

    2024 day 21: simplify the code

--- src/year2024/day21.rs
+++ src/year2024/day21.rs
@@ -27,15 +27,7 @@ fn solve_row(row: &str) -> (u64, u64) {
     (n * p1, n * p2)
 }
 
-const DIRPAD_TABLE: [[u64; 25]; 25] = compute_dirpad_table();
-const NUMPAD_PAIR_WEIGHT2: [u64; 121] = numpad_pair_weights(2);
-const NUMPAD_PAIR_WEIGHT25: [u64; 121] = numpad_pair_weights(25);
-
-const fn numpad_index(c: u8) -> usize {
-    if c == b'A' { 10 } else { (c - b'0') as usize }
-}
-
-const fn mk_numpad() -> [(i32, i32); 128] {
+const NUMPAD: [(i32, i32); 128] = {
     let mut table = [(0, 0); 128];
     table[b'0' as usize] = (1, 0);
     table[b'A' as usize] = (2, 0);
@@ -49,41 +41,30 @@ const fn mk_numpad() -> [(i32, i32); 128] {
     table[b'8' as usize] = (1, 3);
     table[b'9' as usize] = (2, 3);
     table
-}
-
-const NUMPAD: [(i32, i32); 128] = mk_numpad();
-
+};
 const DIRPAD: [(i32, i32); 5] = [(2, 0), (0, 0), (1, 1), (1, 0), (2, 1)];
+const DIRPAD_TABLE: [[u64; 25]; 25] = compute_dirpad_table();
+const NUMPAD_PAIR_WEIGHT2: [u64; 121] = numpad_pair_weights(2);
+const NUMPAD_PAIR_WEIGHT25: [u64; 121] = numpad_pair_weights(25);
 
-const fn numpad_step(from: u8, to: u8) -> (u8, i32, u8, i32) {
-    let (fx,fy) = NUMPAD[from as usize];
-    let (tx,ty) = NUMPAD[to as usize];
+const fn numpad_index(c: u8) -> usize {
+    if c == b'A' { 10 } else { (c - b'0') as usize }
+}
+
+const fn next_sequence(from: u8, to: u8, pad: &[(i32, i32)], hole: (i32, i32)) -> [u64; 25] {
+    let (fx,fy) = pad[from as usize];
+    let (tx,ty) = pad[to as usize];
     let dx = tx - fx;
     let dy = ty - fy;
     let horiz = if dx > 0 {(0, dx)} else {(1, -dx)};
     let vert = if dy > 0 {(2, dy)} else {(3, -dy)};
-    if (dx > 0 && !(fx == 0 && ty == 0)) || tx == 0 && fy == 0  {
-        (vert.0, vert.1, horiz.0, horiz.1)
-    } else {
-        (horiz.0, horiz.1, vert.0, vert.1)
-    }
-}
+    let (d1, f1, d2, f2) =
+        if (dx > 0 && !(fx == hole.0 && ty == hole.1)) || tx == hole.0 && fy == hole.1  {
+            (vert.0, vert.1, horiz.0, horiz.1)
+        } else {
+            (horiz.0, horiz.1, vert.0, vert.1)
+        };
 
-const fn dirpad_step(from: u8, to: u8) -> (u8, i32, u8, i32) {
-    let (fx,fy) = DIRPAD[from as usize];
-    let (tx,ty) = DIRPAD[to as usize];
-    let dx = tx - fx;
-    let dy = ty - fy;
-    let horiz = if dx > 0 {(0, dx)} else {(1, -dx)};
-    let vert = if dy > 0 {(2, dy)} else {(3, -dy)};
-    if (dx > 0 && !(fx == 0 && ty == 1)) || tx == 0 && fy == 1  {
-        (vert.0, vert.1, horiz.0, horiz.1)
-    } else {
-        (horiz.0, horiz.1, vert.0, vert.1)
-    }
-}
-
-const fn sequence_aux(d1: u8, f1: i32, d2: u8, f2: i32) -> [u64; 25] {
     let mut table = [0; 25];
     if f1 == 0 && f2 == 0 {
         table[4 * 5 + 4] = 1;
@@ -129,8 +110,7 @@ const fn compute_dirpad_table() -> [[u64; 25]; 25] {
         let mut to = 0;
         while to < 5 {
             let index = ((from * 5) + to) as usize;
-            let (d1, f1, d2, f2) = dirpad_step(from, to);
-            table[index] = sequence_aux(d1, f1, d2, f2);
+            table[index] = next_sequence(from, to, &DIRPAD, (0, 1));
             to += 1;
 
         }
@@ -180,8 +160,7 @@ const fn dirpad_pair_weights(depth: u32) -> [u64; 25] {
 }
 
 const fn numpad_pair_weight(from: u8, to: u8, dirpad_weights: &[u64]) -> u64 {
-    let (d1, f1, d2, f2) = numpad_step(from, to);
-    let table = sequence_aux(d1, f1, d2, f2);
+    let table = next_sequence(from, to, &NUMPAD, (0, 0));
     let mut res = 0;
     let mut i = 0;
     while i < 25 {
