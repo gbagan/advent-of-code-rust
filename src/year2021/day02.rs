19b94504d26ffd97d5922918f3900a334719067a
Guillaume Bagan <gbagan@gmail.com>
2025-07-25 10:55:56 +0200

    improve 2021 day 2

--- src/year2021/day02.rs
+++ src/year2021/day02.rs
@@ -1,58 +1,32 @@
-use itertools::Itertools;
-use crate::util::parser::*;
-
-enum Command {
-    Forward(i32),
-    Down(i32),
-}
-
-fn parse_command(cmd: &str, amount: &str) -> Command {
-    let amount: i32 = amount.try_unsigned().unwrap();
-    match cmd {
-        "forward" => Command::Forward(amount),
-        "down" => Command::Down(amount),
-        "up" => Command::Down(-amount),
-        _ => panic!("Invalid command {cmd}")
-    }
-}
-
-pub fn solve(input: &str) -> (i32, i32) {
-    let commands: Vec<_> = input
-        .split_ascii_whitespace()
-        .tuples()
-        .map(|(cmd, amount)| parse_command(cmd, amount))
-        .collect();
-
-    let p1 = part1(&commands);
-    let p2 = part2(&commands);
-
-    (p1, p2)
-}
-
-
-fn part1(commands: &[Command]) -> i32 {
+pub fn solve(input: &str) -> (u32, u32) {
+    let mut input = input.as_bytes();
     let mut position = 0;
-    let mut depth = 0;
-    for command in commands {
-        match command {
-            Command::Forward(n) => position += n,
-            Command::Down(n) => depth += n
-        }
-    }
-
-    position * depth
-}
-
-fn part2(commands: &[Command]) -> i32 {
-    let mut position = 0;
-    let mut depth = 0;
+    let mut depth1 = 0;
+    let mut depth2 = 0;
     let mut aim = 0;
-    for command in commands {
-        match command {
-            Command::Forward(n) => { position += n; depth += aim * n },
-            Command::Down(n) => aim += n
+
+    while !input.is_empty() {
+        match input[0] {
+            b'f' => { // forward
+                let n = (input[8] - b'0') as u32;
+                position += n;
+                depth2 += aim * n;
+                input = &input[10..];
+            }
+            b'd' => { // down
+                let n = (input[5] - b'0') as u32;
+                depth1 += n;
+                aim += n;
+                input = &input[7..];
+            }
+            _ => { // up
+                let n = (input[3] - b'0') as u32;
+                depth1 -= n;
+                aim -= n;
+                input = &input[5..];
+            }
         }
     }
 
-    position * depth
+    (position * depth1, position * depth2)
 }
