54c25ceb89b9c8d8c714a00796ebe07797758964
Guillaume Bagan <gbagan@gmail.com>
2025-08-24 00:14:40 +0200

    improve 2024 day 5

--- src/year2024/day05.rs
+++ src/year2024/day05.rs
@@ -1,32 +1,43 @@
-use crate::util::{iter::*, parser::*};
-use std::cmp::Ordering;
-use memchr::memmem;
-
 pub fn solve(input: &str) -> (usize, usize) {
-    let mut table = [Ordering::Greater; 10000];
+    let mut input = input.as_bytes();
+    let mut rules = [0u128; 100];
 
-    let split = memmem::find(input.as_bytes(), b"\n\n").unwrap();
-    // todo
-    let section1 = &input[..split];
-    let section2 = &input[split+2..];
-    for (x, y) in section1.iter_unsigned::<usize>().tuples() {
-        table[x*100+y] = Ordering::Less;
+    while input[0] != b'\n' && input.len() >= 6 {
+        let x = input[0] as usize * 10 + input[1] as usize - 528;
+        let y = input[3] as usize * 10 + input[4] as usize - 528;
+        rules[x] |= 1 << y;
+        input = &input[6..];
     }
 
+    input = &input[1..];
     let mut p1 = 0;
     let mut p2 = 0;
-    let mut row = Vec::new();
+    let mut row = [0; 25];
+    let mut idx = 0;
+    let mut is_sorted = true;
+    let mut mask = 0;
 
-    for line in section2.lines() {    
-        row.clear();
-        row.extend(line.iter_unsigned::<usize>());
-        let n = row.len();
-        if row.is_sorted_by(|x, y| table[x*100+y] == Ordering::Less) {
-            p1 += row[n/2];
-        } else {
-            p2 += *row.select_nth_unstable_by(n/2, |x, y| table[x*100+y]).1;
+    for chunk in input.chunks_exact(3) {
+        let x = chunk[0] as usize * 10 + chunk[1] as usize - 528;
+        row[idx] = x;
+        idx += 1;
+        is_sorted &= rules[x] & mask == 0;
+        mask |= 1 << x; 
+
+        if chunk[2] == b'\n' {
+            if is_sorted {
+                p1 += row[idx/2];
+            } else {
+                for &n in &row[..idx] {
+                    if (rules[n] & mask).count_ones() == (idx/2) as u32 {
+                        p2 += n;
+                    } 
+                }
+            }
+            idx = 0;
+            is_sorted = true;
+            mask = 0;
         }
-
     }
     (p1, p2)
 }
