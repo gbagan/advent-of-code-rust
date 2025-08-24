5207f6a121e1828d1483338b62fec1e73ae1c60b
Guillaume Bagan <gbagan@gmail.com>
2025-08-25 01:57:26 +0200

    improve 2020 day 25

--- src/year2020/day25.rs
+++ src/year2020/day25.rs
@@ -1,46 +1,152 @@
 // discrete logarithm
 // https://en.wikipedia.org/wiki/Baby-step_giant-step
+// https://en.wikipedia.org/wiki/Pohlig%E2%80%93Hellman_algorithm
 
-use ahash::{HashMap, HashMapExt};
+use crate::util::{iter::*, parser::*};
 
-use crate::util::{iter::*, math::*, parser::*};
-
+const MODULO: u64 = 20_201_227;
+// phi(MODULO) = MODULO-1 = 2 * 3 * 29 * 116099
+const BASE: u64 = 7;
 
 pub fn solve(input: &str) -> (u64, u32) {
     let (card_key, door_key) = input.iter_unsigned().next_tuple().unwrap();
-    const MODULO: u64 = 20_201_227;
-    const BASE: u64 = 7;
-    let exponent = discrete_logarithm(BASE, MODULO, card_key);
-    let p1 = door_key.mod_pow(exponent as usize, MODULO);
+    let exponent = discrete_logarithm(card_key);
+    let p1 = mod_pow(door_key, exponent);
 
     (p1, 0)
 }
 
-#[inline(always)]
-fn discrete_logarithm(base: u64, modulo: u64, beta: u64) -> u64 {
-    let m = modulo.isqrt() + 1;
-    let mut table = HashMap::with_capacity(m as usize);
-    let mut b_i = 1;
-    for i in 0..m {
-        table.insert(b_i, i);
-        b_i = (base * b_i) % modulo;
-    }
-    let c = base.mod_pow((m * (modulo - 2)) as usize, modulo);
-
-    let mut y = beta;
-    for i in 0..m {
-        if let Some(&j) = table.get(&y) {
-            return i * m + j;
-        }
-       y = (c * y) % modulo;
-    }
-
-
-    unreachable!();
+fn discrete_logarithm(n: u64) -> u64 {
+	(CHINESE[0] * log_mod_2(n)
+    + CHINESE[1] * log_mod_3(n)
+	+ CHINESE[2] * log_mod_29(n)
+    + CHINESE[3] * log_mod_116099(n)
+    ) % (MODULO - 1)
 }
 
-#[test]
-fn discrete_logarithm_test() {
-    assert_eq!(discrete_logarithm(2, 5, 3), 3);
-    assert_eq!(discrete_logarithm(3, 7, 4), 4);
+fn log_mod_2(n: u64) -> u64 {
+    const EXP: u64 = (MODULO - 1) / 2;
+    (mod_pow(n, EXP) != 1) as u64
+}
+
+fn log_mod_3(n: u64) -> u64 {
+    const EXP: u64 = (MODULO - 1) / 3;
+	mod_pow(n, EXP) >> 5 & 3
+}
+
+fn log_mod_29(n: u64) -> u64 {
+    const EXP: u64 = (MODULO - 1) / 29;
+
+    const fn hash(x: u64) -> usize {
+        (((x >> 8) ^ (x >> 18)) % 53) as usize
+    } 
+
+    const TABLE: [u8; 53] = {
+        const B: u64 = mod_pow(BASE, EXP);
+        let mut table = [u8::MAX; 53];
+        let mut i = 0;
+        let mut x = 1;
+        while i < 29 {
+            table[hash(x)] = i;
+            x = x * B % MODULO;
+            i += 1;
+        }
+        table
+    };
+
+	TABLE[hash(mod_pow(n, EXP))] as u64
+}
+
+fn log_mod_116099(n: u64) -> u64 {
+    const N: u64 = 116_099;
+    const EXP: u64 = (MODULO - 1) / N;
+    const B: u64 = mod_pow(BASE, EXP);
+	const R: u64 = N.isqrt() + 1;
+    const C: u64 = mod_pow(modular_inverse(B, MODULO), R);
+
+	const fn hash(n: u64) -> u64 {
+        ((19*n) ^ (n>>3) ^ (n>>10)) % 2762
+    }
+
+    const EXP_TABLE: [u16; 2762] = {
+        let mut table = [0; 2762];
+        let mut i = 0;
+        let mut z = 1;
+	    while i < R {
+		    let h = hash(z) as usize;
+            table[h] = i as u16;
+            i += 1;
+		    z = z * B % MODULO;
+        }
+        table
+    };
+
+    const POW_TABLE: [u32; 2762] = {
+        let mut table = [0; 2762];
+        let mut i = 0;
+        let mut z = 1;
+	    while i < R {
+		    let h = hash(z) as usize;
+            table[h] = z as u32;
+            i += 1;
+		    z = z * B % MODULO;
+        }
+        table
+    };
+
+	let mut y = mod_pow(n, EXP);
+	for i in 0..R {
+		let h = hash(y) as usize;
+		if POW_TABLE[h] as u64 == y {
+			return i * R + EXP_TABLE[h] as u64;
+		}
+		y = y * C % MODULO;
+	}
+
+	unreachable!();
+}
+
+const CHINESE: [u64; 4] = {
+    let n1 = 2;
+    let n2 = 3;
+    let n3 = 29;
+    let n4 = 116_099;
+    let q1 = (MODULO - 1) / n1;
+    let q2 = (MODULO - 1) / n2;
+    let q3 = (MODULO - 1) / n3;
+    let q4 = (MODULO - 1) / n4;
+    let m1 = modular_inverse(q1, n1);
+    let m2 = modular_inverse(q2, n2);
+    let m3 = modular_inverse(q3, n3);
+    let m4 = modular_inverse(q4, n4);
+
+    [q1 * m1, q2 * m2, q3 * m3, q4 * m4]
+};
+
+const fn mod_pow(mut x: u64, mut n: u64) -> u64 {
+    let mut p = 1;
+    while n > 0 {
+        if n & 1 == 1 {
+            p = (p * x) % MODULO;
+        }
+        x = (x * x) % MODULO;
+        n >>= 1;
+    }
+    p
+}
+
+const fn modular_inverse(n: u64, m: u64) -> u64 {
+    let (mut t, mut t1) = (0, 1);
+    let (mut r, mut r1) = (m as i64, n as i64);
+
+    while r1 != 0 {
+        let q = r / r1;
+        (t, t1) = (t1, t - q * t1);
+        (r, r1) = (r1, r - q * r1);
+    }
+
+    if t < 0 {
+        t += m as i64;
+    }
+    t as u64
 }
