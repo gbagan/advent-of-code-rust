4fadcd387240522df21062922f2c501932ee2b4a
Guillaume Bagan <gbagan@gmail.com>
2025-12-01 11:07:58 +0100

    2025 optimize day 1

--- src/year2025/day01.rs
+++ src/year2025/day01.rs
@@ -1,26 +1,29 @@
-use crate::util::parser::*;
-
 pub fn solve(input: &str) -> (u32, u32) {
-    let input = input.trim().as_bytes();
-    let amounts = input.iter_unsigned::<u32>();
-    let directions = input.iter().filter(|&&c| c.is_ascii_uppercase());
-
-    let mut dial = 1_000_000_050;
+    let mut dir = false;
+    let mut delta = 0;
+    let mut dial = (u32::MAX/2).next_multiple_of(100) + 50;
     let mut p1 = 0;
     let mut p2 = 0;
 
-    for (&dir, amount) in directions.zip(amounts) {
-        if dir == b'L' {
-            let next = dial - amount;
-            p2 += (dial - 1) / 100 - (next - 1) / 100;
-            dial = next;
-        } else {
-            let next = dial + amount;
-            p2 += next / 100 - dial / 100;
-            dial = next;
-        };
-        p1 += (dial % 100 == 0) as u32;
+    for c in input.bytes() {
+        match c {
+            _ if c.is_ascii_digit() => delta = delta * 10 + (c - b'0') as u32,
+            b'\n' => {
+                if dir {
+                    let next = dial + delta;
+                    p2 += next / 100 - dial / 100;
+                    dial = next;           
+                } else {
+                    let next = dial - delta;
+                    p2 += (dial - 1) / 100 - (next - 1) / 100;
+                    dial = next;
+                };
+                p1 += (dial % 100 == 0) as u32;
+                delta = 0;
+            }
+            b'L' => dir = false,
+            _ => dir = true,
+        }
     }
-
     (p1, p2)
 }
