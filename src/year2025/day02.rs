e5481e0ea8933e1206e3f5d1268c7ad28cf90367
Guillaume Bagan <gbagan@gmail.com>
2025-12-02 16:45:43 +0100

    day 2: code cleaning

--- src/year2025/day02.rs
+++ src/year2025/day02.rs
@@ -17,51 +17,51 @@ pub fn solve(input: &str) -> (u64, u64) {
                 2 => {
                     let start = start.max(10);
                     let end = end.min(99);
-                    p1 += sum_invalid_patterns(start, end, 11);
+                    p1 += sum_invalid_ids(start, end, 11);
                 },
                 3 => {
                     let start = start.max(100);
                     let end = end.min(999);
-                    p2 += sum_invalid_patterns(start, end, 111);
+                    p2 += sum_invalid_ids(start, end, 111);
                 }
                 4 => {
                     let start = start.max(1_000);
                     let end = end.min(9_999);
-                    p1 += sum_invalid_patterns(start, end, 101);
+                    p1 += sum_invalid_ids(start, end, 101);
                 },
                 5 => {
                     let start = start.max(10_000);
                     let end = end.min(99_999);
-                    p2 += sum_invalid_patterns(start, end, 11_111);
+                    p2 += sum_invalid_ids(start, end, 11_111);
                 }
                 6 => {
                     let start = start.max(100_000);
                     let end = end.min(999_999);
-                    p1 += sum_invalid_patterns(start, end, 1001);
-                    p2 += sum_invalid_patterns(start, end, 10_101)
-                        - sum_invalid_patterns(start, end, 111_111);
+                    p1 += sum_invalid_ids(start, end, 1001);
+                    p2 += sum_invalid_ids(start, end, 10_101)
+                        - sum_invalid_ids(start, end, 111_111);
                 },
                 7 => {
                     let start = start.max(1_000_000);
                     let end = end.min(9_999_999);
-                    p2 += sum_invalid_patterns(start, end, 1_111_111);
+                    p2 += sum_invalid_ids(start, end, 1_111_111);
                 },
                 8 => {
                     let start = start.max(10_000_000);
                     let end = end.min(99_999_999);
-                    p1 += sum_invalid_patterns(start, end, 10_001);
+                    p1 += sum_invalid_ids(start, end, 10_001);
                 },
                 9 => {
                     let start = start.max(100_000_000);
                     let end = end.min(999_999_999);
-                    p2 += sum_invalid_patterns(start, end, 1_001_001);
+                    p2 += sum_invalid_ids(start, end, 1_001_001);
                 },
                 10 => {
                     let start = start.max(1_000_000_000);
                     let end = end.min(9_999_999_999);
-                    p1 += sum_invalid_patterns(start, end, 100_001);
-                    p2 += sum_invalid_patterns(start, end, 101_010_101)
-                        - sum_invalid_patterns(start, end, 1_111_111_111);
+                    p1 += sum_invalid_ids(start, end, 100_001);
+                    p2 += sum_invalid_ids(start, end, 101_010_101)
+                        - sum_invalid_ids(start, end, 1_111_111_111);
                 },
                 _ => {}
             }
@@ -71,7 +71,7 @@ pub fn solve(input: &str) -> (u64, u64) {
     (p1, p1+p2)
 }
 
-fn sum_invalid_patterns(start: u64, end: u64, mult: u64) -> u64 {
+fn sum_invalid_ids(start: u64, end: u64, mult: u64) -> u64 {
     let first = start.div_ceil(mult);
     let last = end / mult;
     mult * ((first + last) * (last - first + 1) / 2)
